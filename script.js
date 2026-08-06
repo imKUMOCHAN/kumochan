e9e0c847921b490098e7f87a31e6b3e32038882a
imKUMOCHAN <205641260+imKUMOCHAN@users.noreply.github.com>
2026-08-07 01:06:43 +0200

    Finally merging away from ITGOESDEEPER only (please email me at 'kumochan@kumochan.com' if you discover any bugs <3)

--- script.js
+++
@@ -1,8 +0,0 @@
-function loadPage() {
-    document.getElementById("loading").style.display = "flex";
-
-    // Wait 2 seconds before opening the page
-    setTimeout(() => {
-        window.location.href = "page2.html";
-    }, 2000);
-}
